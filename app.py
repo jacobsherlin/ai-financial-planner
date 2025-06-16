a58b5b89e70425d163095debcb0f34bd8c799e32
Jacob Sherlin <jms2jn@mtmail.mtsu.edu>
2025-06-15 20:09:21 -0500

    Update app.py fixed port issue

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))  # For local development only
